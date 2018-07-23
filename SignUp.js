e9ce27a64717b8a486bb9811baa9e81316577941
Andrew Marion <31940242+Ealm96@users.noreply.github.com>
2018-07-23 14:50:11 -0400

    A budding website to celebrate Dungeons and Dragons as well as a place for people to meet and start games together.

---
+++ SignUp.js
@@ -0,0 +1,9 @@
+function myFunction(){
+  var usrnm = document.forms["myForm"]["usrnm"].value;
+  var p1 = document.forms["myForm"]["psw"].value;
+  var p2 = document.forms["myForm"]["psw-repeat"].value;
+  if(p1 != p2){
+    alert('Passwords do not match!');
+    return false;
+  }
+}
