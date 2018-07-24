1c491c1264b7b298af356b0c882c722389b016e9
Andrew Marion <31940242+Ealm96@users.noreply.github.com>
2018-07-24 15:28:38 -0400

    messing around

--- SignUp.js
+++ SignUp.js
@@ -3,18 +3,7 @@ function myFunction(){
   var usrnm = document.forms["myForm"]["usrnm"].value;
   var p1 = document.forms["myForm"]["psw"].value;
   var p2 = document.forms["myForm"]["psw-repeat"].value;
-  var pr = document.getElementById("pswd-r");
-  var usnm = document.getElementById("usnm");
-  var p = document.getElementById("pswd");
-  if (usrnm == usnm.placeholder) {
-    usnm.style.backgroundColor = "red";
-  }
-  if (p1 == p1.placeholder) {
-    p.style.backgroundColor = "red";
-  }
-  if (p2 == p2.placeholder) {
-    pr.style.backgroundColor = "red";
-  }
+
   if(p1 != p2){
     alert('Passwords do not match!');
   }
